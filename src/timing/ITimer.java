5dba541a34b9184edcc7b7ae0873630627fccdd4
Raul <iacoban.raul08@gmail.com>
2022-03-20 22:36:17 +0200

    CU-18 Added getters, unit conversion

--- src/timing/ITimer.java
+++ src/timing/ITimer.java
@@ -1,8 +1,18 @@
 package timing;
 
 public interface ITimer {
-    public void start();
-    public long stop();
-    public void resume();
-    public long pause();
+    void start();
+    long stop();
+    void resume();
+    long pause();
+    /**
+     * @param unit time unit used for measurement
+     * @return total running time between start() and the last call of pause() or stop()
+     */
+    double getTime(TimeUnit unit);
+    /**
+     * @param unit time unit used for measurement
+     * @return running time between last call of start() or resume() and the last call of pause() or stop()
+     */
+    double getElapsedTime(TimeUnit unit);
 }
--- src/timing/Timer.java
+++ src/timing/Timer.java
@@ -3,20 +3,22 @@ package timing;
 public class Timer implements ITimer{
 	private long total_t;
 	private long start_t;
+    private long elapsed_t;
 
     public Timer(){
-        
+
     }
 
     public void start(){
         this.total_t = 0;
+        this.elapsed_t = 0;
         this.start_t = System.nanoTime();
     }
 
     public long stop(){
-    	long stop_t = System.nanoTime();
-    	this.total_t += stop_t - this.start_t;
-    	
+        this.elapsed_t = System.nanoTime() - this.start_t;
+    	this.total_t += this.elapsed_t;
+
         return total_t;
     }
 
@@ -25,10 +27,17 @@ public class Timer implements ITimer{
     }
 
     public long pause(){
-    	long pause_t = System.nanoTime();
-    	long elapsed_t = pause_t - this.start_t;
-    	this.total_t += elapsed_t;
+    	this.elapsed_t = System.nanoTime() - this.start_t;
+    	this.total_t += this.elapsed_t;
     	
         return elapsed_t;
+    }
+
+    public double getTime(TimeUnit unit){
+        return (double)this.total_t / unit.getValue();
+    }
+
+    public double getElapsedTime(TimeUnit unit){
+        return (double)this.elapsed_t / unit.getValue();
     }
 }
