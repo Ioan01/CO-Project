e0398997618ca529a619aef269d997a5d9db38c1
Andrei J <andrei.joldea@student.upt.ro>
2022-05-11 17:22:32 +0300

    Added fields for path and sizes + conversion to bytes

--- src/main/java/upt/coproject/HDD_Controller.java
+++ src/main/java/upt/coproject/HDD_Controller.java
@@ -22,41 +22,44 @@ import java.util.ResourceBundle;
 public class HDD_Controller extends Controller implements Initializable {
 
     @FXML
-    private AnchorPane anchorid;
+    private AnchorPane anchorID;
     @FXML
-    private ProgressBar pb_progress_hdd;
+    private ProgressBar progressBarProgressHDD;
     @FXML
-    private Button button_checkResults;
+    private Button buttonCheckResults;
     @FXML
     private final MockBench testBench;
     @FXML
-    private TextField tf_path;
+    private TextField textFieldPath;
     @FXML
-    private Button button_browse, button_start, button_cancel;
+    private Button button_browse, buttonStart, buttonCancel;
     @FXML
-    private ComboBox<String> cb_ioSize_1 , cb_ioSize_2, cb_fileSize;
+    private ComboBox<String> comboBoxIOSizeStart, comboBoxIOSizeEnd, comboBoxFileSize;
 
-    private ObservableList<String> io_list = FXCollections.observableArrayList("512 B", "1 KB", "2 KB", "4 KB", "8 KB",
+    private ObservableList<String> ioList = FXCollections.observableArrayList("512 B", "1 KB", "2 KB", "4 KB", "8 KB",
             "16 KB", "32 KB", "64 KB", "128 KB", "256 KB", "512 KB", "1 MB", "2 MB", "4 MB", "8 MB", "16 MB", "32 MB", "64 MB");
-    private  ObservableList<String> fs_list = FXCollections.observableArrayList("64 KB", "128 KB", "256 KB", "512 KB",
+    private  ObservableList<String> fsList = FXCollections.observableArrayList("64 KB", "128 KB", "256 KB", "512 KB",
             "1 MB", "2 MB","4 MB", "8 MB", "16 MB", "32 MB", "64 MB", "128 MB", "256 MB", "512 MB", "1 GB", "2 GB", "4 GB",
             "8 GB", "16 GB", "32 GB");
     @FXML
-    private Text text_progress_hdd_tracker;
+    private Text textHDDProgressTracker;
+
+    private String drivePath;
+    private long fileSize, ioStart, ioEnd; // bytes
 
     public HDD_Controller()
     {
 
-        testBench = new MockBench();
+        testBench = new MockBench("");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        cb_ioSize_1.setItems(io_list);
-        cb_ioSize_2.setItems(io_list);
-        cb_fileSize.setItems(fs_list);
-        button_checkResults.setVisible(false);
-        button_cancel.setVisible(false);
+        comboBoxIOSizeStart.setItems(ioList);
+        comboBoxIOSizeEnd.setItems(ioList);
+        comboBoxFileSize.setItems(fsList);
+        buttonCheckResults.setVisible(false);
+        buttonCancel.setVisible(false);
 
     }
 
@@ -64,35 +67,36 @@ public class HDD_Controller extends Controller implements Initializable {
     {
 
         final DirectoryChooser directoryChooser = new DirectoryChooser();
-        Stage stage = (Stage)anchorid.getScene().getWindow();
+        Stage stage = (Stage) anchorID.getScene().getWindow();
         File file = directoryChooser.showDialog(stage);
 
         if(file != null)
         {
-            System.out.println("Path : "+file.getAbsolutePath());
-            tf_path.setText(file.getAbsolutePath());
+            textFieldPath.setText(file.getAbsolutePath());
+            drivePath = file.getAbsolutePath();
+            System.out.println("Chosen path: "+drivePath);
         }
     }
 
     public void start_HDD(ActionEvent event)
     {
-        pb_progress_hdd.progressProperty().bind(testBench.getRunningProgress());
+        progressBarProgressHDD.progressProperty().bind(testBench.getRunningProgress());
 
 
-        pb_progress_hdd.progressProperty().addListener(new ChangeListener<Number>() {
+        progressBarProgressHDD.progressProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                text_progress_hdd_tracker.textProperty().setValue(Integer.toString((int)(number.doubleValue()*100) )+ '%');
-                if(text_progress_hdd_tracker.getText().equals("99%"))
+                textHDDProgressTracker.textProperty().setValue(Integer.toString((int)(number.doubleValue()*100) )+ '%');
+                if(textHDDProgressTracker.getText().equals("99%"))
                 {
-                    button_checkResults.setVisible(true);
+                    buttonCheckResults.setVisible(true);
                 }
             }
         });
         testBench.initialize(10);
         testBench.start();
-        button_start.setVisible(false);
-        button_cancel.setVisible(true);
+        buttonStart.setVisible(false);
+        buttonCancel.setVisible(true);
     }
     public void stop_HDD(ActionEvent event)
     {
@@ -100,6 +104,44 @@ public class HDD_Controller extends Controller implements Initializable {
         //text_progress_hdd_tracker.textProperty().setValue("");
         //pb_progress_hdd.progressProperty().set(0);
     }
+    public void ioStartSizeSelected(ActionEvent event)
+    {
+        String size = comboBoxIOSizeStart.getValue();
+        ioStart = convertToBytes(size);
+        System.out.println("IO start size selected: "+ioStart+" bytes");
+        //if(sizeString.equals())
+    }
+    public void ioEndSizeSelected(ActionEvent event)
+    {
+        String size = comboBoxIOSizeEnd.getValue();
+        ioEnd = convertToBytes(size);
+        System.out.println("IO end size selected: "+ioEnd+" bytes");
+    }
 
+    public void fileSizeSelected(ActionEvent event)
+    {
+        String size = comboBoxFileSize.getValue();
+        fileSize = convertToBytes(size);
+        System.out.println("File size selected: "+fileSize+" bytes");
+    }
 
+    public long convertToBytes(String size)
+    {
+        long result = 1;
+        if(size != null) {
+            String[] arr = size.split(" ");
+            long value = Long.valueOf(arr[0]);
+            String unit = arr[1];
+            if (unit.equals("B"))
+                result = value;
+            else if (unit.equals("KB"))
+                result = value * 1024;
+            else if (unit.equals("MB"))
+                result = value * 1048576;
+            else if (unit.equals("GB"))
+                result = value * 1073741824;
+        }
+
+        return result;
+    }
 }
