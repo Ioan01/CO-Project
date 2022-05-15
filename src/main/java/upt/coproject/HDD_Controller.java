fefef031c66865c8ed752353b2f14f694e095a9d
Andrei J <andrei.joldea@student.upt.ro>
2022-05-15 21:43:00 +0300

    HDD model detection Update

--- src/main/java/upt/coproject/HDD_Controller.java
+++ src/main/java/upt/coproject/HDD_Controller.java
@@ -4,6 +4,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,11 +13,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import lombok.SneakyThrows;
+import org.w3c.dom.ls.LSOutput;
 import upt.coproject.benchmark.MockBench;
 import upt.coproject.testbench.DriveTestBench;
 
-import java.io.File;
+import java.io.*;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
@@ -46,20 +50,24 @@ public class HDD_Controller extends Controller implements Initializable {
     private Text textHDDProgressTracker, textRandomWriteSpeed, textRandomReadSpeed, textSeqWriteSpeed, textSeqReadSpeed;
     @FXML
     private Label labelSeqReadSpeed, labelSeqWriteSpeed, labelRandomReadSpeed, labelRandomWriteSpeed;
+    @FXML
+    private  Text textHDDModel;
 
-
-    private String drivePath;
+    private File drivePath;
     private long fileSize, ioStart, ioEnd; // bytes
+    private String hddModel = "";
 
     public HDD_Controller()
     {
 
         testBench = new DriveTestBench();
-
     }
 
+    @SneakyThrows
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        getHDDModel();
+        textHDDModel.setText("");
         comboBoxIOSizeStart.setItems(ioList);
         comboBoxIOSizeEnd.setItems(ioList);
         comboBoxFileSize.setItems(fsList);
@@ -76,89 +84,73 @@ public class HDD_Controller extends Controller implements Initializable {
 
     }
 
-    public void chooseDirectory(ActionEvent event)
-    {
-
+    public void chooseDirectory(ActionEvent event){
         final DirectoryChooser directoryChooser = new DirectoryChooser();
         Stage stage = (Stage) anchorID.getScene().getWindow();
-        File file = directoryChooser.showDialog(stage);
+        drivePath = directoryChooser.showDialog(stage);
 
-
-        if(file != null)
+        if(drivePath != null)
         {
-            textFieldPath.setText(file.getAbsolutePath());
-            drivePath = file.getAbsolutePath();
-            System.out.println("Chosen path: "+drivePath);
+            textFieldPath.setText(drivePath.getAbsolutePath());
+            System.out.println("Chosen path: "+drivePath.getAbsolutePath());
+            textHDDModel.setText(hddModel);
         }
     }
 
     public void startHDD(ActionEvent event)
     {
-
-        progressBarProgressHDD.progressProperty().bind(testBench.getRunningProgress());
-
-
-        testBench.getFinished().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
-                if (t1)
-                {
-                    buttonCancel.setVisible(false);
-                    buttonStart.setVisible(true);
-                    buttonCancel.setVisible(false);
-                    buttonStart.setVisible(true);
-                    buttonCheckResults.setVisible(true);
-                    labelRandomWriteSpeed.setVisible(true);
-                    labelRandomReadSpeed.setVisible(true);
-                    labelSeqReadSpeed.setVisible(true);
-                    labelSeqWriteSpeed.setVisible(true);
-                    textRandomWriteSpeed.setVisible(true);
-                    textRandomReadSpeed.setVisible(true);
-                    textSeqReadSpeed.setVisible(true);
-                    textSeqWriteSpeed.setVisible(true);
-                    setSeqWriteSpeed((Double) testBench.getResults().get("SEQ_WRITE"));
-                }
-            }
-        });
-
-        testBench.initialize(drivePath,ioStart,ioEnd, (int) fileSize);
-
-        testBench.start();
-
-
-        buttonCancel.setVisible(true);
-        buttonStart.setVisible(false);
-
-        if(file != null)
+        if(drivePath == null || !drivePath.exists())
         {
-            textFieldPath.setText(file.getAbsolutePath());
-            drivePath = file.getAbsolutePath();
-            System.out.println("Chosen path: "+drivePath);
+            displayError("Invalid path!");
         }
-    }
+        else if(ioStart == 0 || ioEnd == 0)
+        {
+            displayError("Select start and end size!");
+        }
+        else if(ioStart > ioEnd)
+        {
+            displayError("Starting size must be smaller!");
+        }
+        else if(fileSize == 0)
+        {
+            displayError("Select file size!");
+        }
+        else {
+            progressBarProgressHDD.progressProperty().bind(testBench.getRunningProgress());
 
-    public void startHDD(ActionEvent event)
-    {
-        progressBarProgressHDD.progressProperty().bind(testBench.getRunningProgress());
 
-
-        progressBarProgressHDD.progressProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                textHDDProgressTracker.textProperty().setValue(Integer.toString((int)(number.doubleValue()*100) )+ '%');
-                if(textHDDProgressTracker.getText().equals("99%"))
-                {
-                    buttonCheckResults.setVisible(true);
+            testBench.getFinished().addListener(new ChangeListener<Boolean>() {
+                @Override
+                public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
+                    if (t1) {
+                        buttonCancel.setVisible(false);
+                        buttonStart.setVisible(true);
+                        buttonCancel.setVisible(false);
+                        buttonStart.setVisible(true);
+                        buttonCheckResults.setVisible(true);
+                        labelRandomWriteSpeed.setVisible(true);
+                        labelRandomReadSpeed.setVisible(true);
+                        labelSeqReadSpeed.setVisible(true);
+                        labelSeqWriteSpeed.setVisible(true);
+                        textRandomWriteSpeed.setVisible(true);
+                        textRandomReadSpeed.setVisible(true);
+                        textSeqReadSpeed.setVisible(true);
+                        textSeqWriteSpeed.setVisible(true);
+                        setSeqWriteSpeed((Double) testBench.getResults().get("SEQ_WRITE"));
+                    }
                 }
-            }
-        });
-        testBench.initialize(10);
-        testBench.start();
-        buttonStart.setVisible(false);
-        buttonCancel.setVisible(true);
+            });
+
+            testBench.initialize(drivePath.getAbsolutePath(), ioStart, ioEnd, (int) fileSize);
+
+            testBench.start();
+
+
+            buttonCancel.setVisible(true);
+            buttonStart.setVisible(false);
+        }
+
     }
-  
-  
     public void stopHDD(ActionEvent event)
     {
         testBench.cancel();
@@ -166,7 +158,6 @@ public class HDD_Controller extends Controller implements Initializable {
         //pb_progress_hdd.progressProperty().set(0);
         buttonCancel.setVisible(false);
         buttonStart.setVisible(true);
-
     }
     public void ioStartSizeSelected(ActionEvent event)
     {
@@ -230,31 +221,23 @@ public class HDD_Controller extends Controller implements Initializable {
     public double setRandomWriteSpeed(double speed) // MB/s
     {
         double result = 1;
-    }
-
-    public void fileSizeSelected(ActionEvent event)
-    {
-        String size = comboBoxFileSize.getValue();
-        fileSize = convertToBytes(size);
-        System.out.println("File size selected: "+fileSize+" bytes");
-    }
-
-    public long convertToBytes(String size)
-    {
-        long result = 1;
-        if(size != null) {
-            String[] arr = size.split(" ");
-            long value = Long.valueOf(arr[0]);
-            String unit = arr[1];
-            if (unit.equals("B"))
-                result = value;
-            else if (unit.equals("KB"))
-                result = value * 1024;
-            else if (unit.equals("MB"))
-                result = value * 1048576;
-            else if (unit.equals("GB"))
-                result = value * 1073741824;
-        }
         return result;
+    }
+
+    public void getHDDModel() throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        String[] cmd = {"cmd","/c", "wmic diskdrive get model"};
+        Process proc = rt.exec(cmd);
+
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        String res;
+        while((res = stdInput.readLine()) != null)
+        {
+            hddModel += res;
+        }
+        hddModel = hddModel.trim().replaceAll(" +", " ");
+        hddModel = hddModel.split(" ")[1];
     }
 }
