b8fff1eb202d9b4a99e210f4164a5af0c8736a7d
Andrei J <andrei.joldea@student.upt.ro>
2022-05-18 00:12:40 +0300

    changed main scene fxml + styling

--- src/main/java/upt/coproject/MainPageController.java
+++ src/main/java/upt/coproject/MainPageController.java
@@ -1,26 +1,105 @@
 package upt.coproject;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class MainPageController extends Controller{
+public class MainPageController extends Controller implements Initializable {
+
+    @FXML
+    private AnchorPane opacityPane,drawerPane;
+
+    @FXML
+    private Label drawerImage;
+
+    @FXML
+    private ImageView exit;
+    @FXML
+    private JFXButton jfxButtonHDD;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        exit.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+
+        opacityPane.setVisible(false);
+
+        FadeTransition fadeTransition=new FadeTransition(Duration.seconds(0.5),opacityPane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        TranslateTransition translateTransition=new TranslateTransition(Duration.seconds(0.5),drawerPane);
+        translateTransition.setByX(-600);
+        translateTransition.play();
+
+        drawerImage.setOnMouseClicked(event -> {
+
+
+            opacityPane.setVisible(true);
+
+            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),opacityPane);
+            fadeTransition1.setFromValue(0);
+            fadeTransition1.setToValue(0.15);
+            fadeTransition1.play();
+
+            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5), drawerPane);
+            translateTransition1.setByX(+600);
+            translateTransition1.play();
+        });
+
+        opacityPane.setOnMouseClicked(event -> {
+
+
+
+            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),opacityPane);
+            fadeTransition1.setFromValue(0.15);
+            fadeTransition1.setToValue(0);
+            fadeTransition1.play();
+
+            fadeTransition1.setOnFinished(event1 -> {
+                opacityPane.setVisible(false);
+            });
+
+
+            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),drawerPane);
+            translateTransition1.setByX(-600);
+            translateTransition1.play();
+        });
+    }
 
     @FXML
     public void goToHDDPage(ActionEvent event){
         //getWindow().setUserData("1000");
-        changePage("scene_hdd.fxml");
-    }
-
-    @FXML
-    public void goToSSDPage(ActionEvent event){
-        //getWindow().setUserData("2000");
-        changePage("scene_ssd.fxml");
+        changePage("scene_hdd.fxml", 1200, 600);
     }
 
     @FXML
     public void goToRAMPage(ActionEvent event){
         //getWindow().setUserData("3000");
         changePage("scene_ram.fxml");
+    }
+    @FXML
+    public void goToResultsPage(ActionEvent event)
+    {
+        changePage("result.fxml");
+    }
+    @FXML
+    public void exitApp(ActionEvent event)
+    {
+        System.exit(0);
     }
 }
