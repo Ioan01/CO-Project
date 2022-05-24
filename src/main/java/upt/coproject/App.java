9040da54eee78fd9cf7ff8b96b857e46293cac89
Andrei J <andrei.joldea@student.upt.ro>
2022-05-24 21:42:30 +0300

    running status

--- src/main/java/upt/coproject/App.java
+++ src/main/java/upt/coproject/App.java
@@ -2,6 +2,7 @@ package upt.coproject;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class App extends Application
 {
@@ -12,6 +13,7 @@ public class App extends Application
         Controller.setWindow(primaryStage);
         primaryStage.setTitle("Gândăceii Pofticioși™");
         primaryStage.setResizable(false);
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
         Controller.changePage("Dashboard.fxml");
 
     }
